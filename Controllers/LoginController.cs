d0df52ae297698cbf73a4f0f834daa7cf9ba4356
Danilo Azevedo Santos <danilo.azevedo@ltm.digital>
2021-05-04 08:17:17 -0300

    initial

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // GET: api/<LoginController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<LoginController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Models.Login login)
+        {
+            if (login == null)
+            {
+                return NoContent();
+            }
+            else
+            {
+                if (login.Usuario == "admin" && login.Senha == "1234")
+                    return Ok();
+                else
+                    return BadRequest();
+            }
+        }
+       
+    }
+}
